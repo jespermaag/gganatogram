1cad570701345e8de81f45a8e963f0c020d9eccc
muschellij2 <muschellij2@gmail.com>
2018-09-19 00:10:38 -0400

    Adding in the mouse.

--- R/gganatogram.R
+++ R/gganatogram.R
@@ -28,6 +28,9 @@
 #' gganatogram(fillOutline='#a6bddb', organism='human',
 #' sex='male', fill="colour")
 #'
+#' gganatogram(fillOutline='#a6bddb', organism='human',
+#' sex='female', fill="colour")
+#'
 #'
 #' #To add organs, create a data frame with specified tissues
 #'
@@ -98,7 +101,13 @@ gganatogram <- function(
     anatogram = NULL) {
 
     if (is.null(anatogram)) {
+        organism = tolower(organism)
+        organism = match.arg(
+            organism,
+            choices = c("human", "mouse"))
         if (organism == 'human') {
+            sex = tolower(sex)
+            sex = match.arg(sex, choices = c("male", "female"))
             if (sex == 'male') {
                 anatogram <- gganatogram::hgMale_list
                 anatogram$outline <- anatogram$human_male_outline
